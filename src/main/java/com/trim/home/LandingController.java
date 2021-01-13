04240f5f1281d46d0a76bd89e1cda945525f91a8
code5887 <saurabh.pradhan@ibm.com@Saurabhs-MacBook-Pro.local>
2021-01-13 17:17:21 -0600

    new updates latest

--- src/main/java/com/trim/home/LandingController.java
+++ src/main/java/com/trim/home/LandingController.java
@@ -1,4 +1,4 @@
-package com.pratik.springsql;
+package com.trim.home;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/trim/home/SpringBootTrialApplicationTests.java
+++ src/test/java/com/trim/home/SpringBootTrialApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pratik.springsql;
+package com.trim.home;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
