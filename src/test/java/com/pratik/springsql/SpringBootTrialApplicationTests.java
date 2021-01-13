d3e6bc257b22bbd41846305f1e6c409b207c7114
Saurabh-Pradhan <saurabh.pradhan@ibm.com@Saurabhs-MacBook-Pro.local>
2021-01-13 17:09:07 -0600

    new updates

--- src/test/java/com/pratik/springsql/SpringBootTrialApplicationTests.java
+++ src/test/java/com/pratik/springsql/SpringBootTrialApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.pratik.springsql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBootTrialApplicationTests {
 
 	@Test
 	void contextLoads() {
